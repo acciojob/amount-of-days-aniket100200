63fe618eb74758044ccb23bf2616d124ef2d7371
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-09 11:04:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function daysOfAYear(year)
+{
+	if(year%4==0)
+	{
+		//Then It suppose to be a leap year and 366 day's..
+		//but condition is ther if it is century year then check it if it is divisible by 400 or not
+
+		if(year%100==0 && year%400!=0)return false;
+
+		return true;
+	}
+}
+
+let year=prompt("Enter the Year");
+
+alert(daysOfAYear(year)?366:365);
+
